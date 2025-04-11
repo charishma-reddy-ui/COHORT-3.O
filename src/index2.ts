2f9833b5ae3fbfb118f87a22b561ec4e1b24b082
charishma-reddy-ui <charishmavadla05@gmail.com>
2025-04-11 18:14:24 +0530

    modified file-2

--- src/index2.ts
+++ src/index2.ts
@@ -2,3 +2,27 @@ function greet(firstName:any){
     console.log(`Hello, ${firstName}!`);
 }
 greet("John");
+interface User{
+    firstName:string;
+    lastName:string;
+    age:number;
+
+}
+function filterUsers(users:User[]){
+    let ans = []
+    for (let i =0;i<users.length;i++){
+        if (users[i].age>18){
+            ans.push(users[i])
+        }
+    }
+    return ans
+}
+let users = [
+    {firstName:"John",lastName:"Doe",age:25},
+    {firstName:"Jane",lastName:"Doe",age:17},
+    {firstName:"Jim",lastName:"Beam",age:30}
+]
+let filteredUsers = filterUsers(users)
+console.log(filteredUsers)
+    
+
